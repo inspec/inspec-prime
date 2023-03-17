e0617ae49a6aa0c3d65ed19fadb9f482d44aa03b
Nikita Mathur <nikita.mathur@chef.io>
2023-03-17 17:15:39 +0530

    Removed logger configuration for chef licensing
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- lib/inspec/base_cli.rb
+++ lib/inspec/base_cli.rb
@@ -64,7 +64,6 @@ module Inspec
       ChefLicensing.configure do |config|
         config.chef_product_name = "Inspec"
         config.chef_entitlement_id = "3ff52c37-e41f-4f6c-ad4d-365192205968"
-        config.logger = Logger.new($stdout)
       end
     end
 
