405f78136fb4b64053a230b216e4e2b2dd489393
Sonu Saha <sonu.saha@progress.com>
2023-03-30 19:56:53 +0530

    EXEC_NAME is always inspec as set in Dist, check is not required
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/plugins/inspec-license/lib/inspec-license.rb
+++ lib/plugins/inspec-license/lib/inspec-license.rb
@@ -1,14 +1,11 @@
 module InspecPlugins
   module License
     class Plugin < ::Inspec.plugin(2)
-      include Inspec::Dist
       plugin_name :"inspec-license"
 
-      if EXEC_NAME == "inspec"
-        cli_command :license do
-          require_relative "inspec-license/cli"
-          InspecPlugins::License::CLI
-        end
+      cli_command :license do
+        require_relative "inspec-license/cli"
+        InspecPlugins::License::CLI
       end
     end
   end
